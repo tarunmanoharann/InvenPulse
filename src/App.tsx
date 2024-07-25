3cf0b7973b821588203882bd8ec918c28318f02b
tarunmanoharann <ceo.tarunmanoharan@gmail.com>
2024-07-25 23:03:27 +0530

    beta version
    creating page struture alone

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import AdminRoutes from ''
+import UserRoutes from './routes/UserRoutes'
+import Login from '../src/components'
+import Register from './pages/Register'
 
-function App() {
+const App: React.FC = () => {
   return (
-    <>
-    </>
+    <div className="min-h-screen bg-gray-100">
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/admin/*" element={<AdminRoutes />} />
+        <Route path="/*" element={<UserRoutes />} />
+      </Routes>
+    </div>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { Provider } from 'react-redux'
+import { BrowserRouter } from 'react-router-dom'
+import App from './App'
+import store from './store'
+import './assets/styles/index.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>
 )
