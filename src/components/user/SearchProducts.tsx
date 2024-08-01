9dedb2b6bbedc261480becb116c19a01ae493f26
tarunmanoharann <ceo.tarunmanoharan@gmail.com>
2024-08-01 10:36:36 +0530

    Update SearchProducts.tsx
    basic structure

---
+++ src/components/user/SearchProducts.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+
+const SearchProducts: React.FC =()=>{
+    return(
+        <div className="p-6">
+            <h1 className="text-3xl"></h1>
+
+        </div>
+    )
+}
+
+export default SearchProducts
