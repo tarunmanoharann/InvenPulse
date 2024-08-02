4e485b682ab83f7f7ae1953b39cd2230e183156c
tarunmanoharann <ceo.tarunmanoharan@gmail.com>
2024-08-02 09:48:56 +0530

    backend
    initializing a maven project

---
+++ backend/invenpulse/src/test/java/com/example/invenpulse/InvenpulseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.invenpulse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvenpulseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
